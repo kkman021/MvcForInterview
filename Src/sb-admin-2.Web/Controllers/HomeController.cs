126a7a0ab962583a24c73af0927ad4b5b4c20211
Ming 楊明勳 <ming@miniasp.com>
2019-08-07 21:52:39 +0800

    新增功能

--- Src/sb-admin-2.Web/Controllers/HomeController.cs
+++ Src/sb-admin-2.Web/Controllers/HomeController.cs
@@ -43,5 +43,45 @@ namespace sb_admin_2.Web.Controllers
 
             return Json(data);
         }
+
+        public ActionResult Create()
+        {
+            var model = new Create();
+
+            return View(model);
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult Create(Create model)
+        {
+            if (!ModelState.IsValid)
+            {
+                NotifyError("有資料異常，請檢核");
+                return View(model);
+            }
+
+            if (_db.Employees.Any(x => x.No == model.No.Trim()))
+            {
+                ModelState.AddModelError(nameof(model.No), "員工編號重覆");
+                NotifyError("員工編號重覆，請重新輸入");
+                return View(model);
+            }
+
+            var entity = new Employee()
+            {
+                ChtName = model.ChtName.Trim(),
+                EngName = model.EngName.Trim(),
+                Email = model.Email.Trim(),
+                No = model.No.Trim()
+                // 這邊不用每次寫 新增時間、人、更新時間、人，因為 SaveChanges 那邊寫好統一處理了
+            };
+
+            _db.Employees.Add(entity);
+            _db.SaveChanges(EmployeeId);
+
+            NotifySuccess("新增成功");
+            return RedirectToAction("Index");
+        }
     }
 }
---
+++ Src/sb-admin-2.Web/Models/Employee/Create.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace sb_admin_2.Web.Models.Employee
+{
+    public class Create
+    {
+        [Required]
+        [DisplayName("員工編號")]
+        public string No { get; set; }
+
+        [Required]
+        [DisplayName("中文姓名")]
+        public string ChtName { get; set; }
+
+        [Required]
+        [DisplayName("英文姓名")]
+        public string EngName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [DisplayName("Email")]
+        public string Email { get; set; }
+    }
+}
